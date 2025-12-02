2249984aab47b46193d51cccd55c8e734dd09237
Linus Johansson <linus.johansson@evolvit.se>
2025-12-02 17:50:40 +0100

    Adds initial Advent of Code project structure
    Sets up the basic project structure for Advent of Code solutions, including:
    
    - A .gitignore file based on the VisualStudio.gitignore template
    - A solution file to manage multiple day projects
    - Initial projects for Day1 and Day2 with basic program files and input files

---
+++ Day1/Program.cs
@@ -0,0 +1,65 @@
+﻿string rawInput = File.ReadAllText("input.txt");
+
+var splitInput = rawInput.Split("\n");
+
+DialProcess dial = new DialProcess();
+
+
+List<RotationEvent> events = new List<RotationEvent>();
+
+foreach (var rotation in splitInput)
+{
+    events.Add(new RotationEvent {Direction = rotation[0].ToString(), Amount = int.Parse(rotation.Substring(1))});
+}
+
+foreach (RotationEvent ev in events)
+{
+    if (ev.Direction == "R") dial.ToBeChanged = ev.Amount;
+    else dial.ToBeChanged = 0 - ev.Amount;
+
+    dial = ToBeChanged(dial);
+    if (dial.Dial == 0) dial.PartOneZeroHits++;
+    Console.WriteLine($"Rotation result = {dial.Dial}");
+}
+Console.WriteLine($"Part one result {dial.PartOneZeroHits}");
+Console.WriteLine($"Part two result {dial.ZeroHits}");
+
+static DialProcess ToBeChanged(DialProcess dial)
+{
+    if (dial.ToBeChanged > 0)
+    {
+        for (int i = dial.ToBeChanged; i != 0; i--)
+        {
+            dial.Dial += 1;
+            if (dial.Dial == 100) dial.Dial = 0;
+            
+            if (dial.Dial == 0) dial.ZeroHits++;
+        }
+    }
+    else
+    {
+        for (int i = dial.ToBeChanged; i != 0; i++)
+        {
+            dial.Dial -= 1;
+            if (dial.Dial == -1) dial.Dial = 99;
+            
+            if (dial.Dial == 0) dial.ZeroHits++;
+        }
+    }
+    
+    return dial;
+}
+
+class RotationEvent
+{
+    public string Direction;
+    public int Amount;
+}
+
+class DialProcess
+{
+    public int Dial = 50;
+    public int ToBeChanged = 0;
+    public int ZeroHits = 0;
+    public int PartOneZeroHits = 0;
+}
