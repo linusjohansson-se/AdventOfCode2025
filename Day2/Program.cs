55269c9eebb9794a5dd636966eb903976f1d94c3
Linus Johansson <GitHub@ontheline.nu>
2025-12-03 20:33:12 +0100

    Solves part two of day two
    Changes the calculation and logic to solve day two of Advent of Code.
    
    Updates the input file to be much larger, and now determines the repeating substrings of the range of numbers provided by the input file.
    Also changes the data types being used to long, instead of int.

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -5,52 +5,61 @@ List<ProcessIds> idRanges = new List<ProcessIds>();
 
 foreach (var range in ranges)
 {
-    idRanges.Add(new ProcessIds {StartingId = int.Parse(range.Split("-")[0]), EndId = int.Parse(range.Split("-")[1])});
+    idRanges.Add(new ProcessIds {StartingId = long.Parse(range.Split("-")[0]), EndId = long.Parse(range.Split("-")[1])});
 }
 
-int sum = 0;
+long sum = 0;
 
 foreach (var idRange in idRanges)
 {
     while (idRange.StartingId <= idRange.EndId)
     {
         var repeatingDigits = HasRepeatingPattern(idRange.StartingId);
-        sum += repeatingDigits.Where(x => x.Value > 1).Sum(x => x.Value);
+        sum += repeatingDigits.Sum(x => long.Parse(x.Key) * x.Value);
         idRange.StartingId++;
     }
 }
 
-Console.WriteLine($"Part one result {sum}");
+Console.WriteLine($"Part two result {sum}");
 
-static Dictionary<string, int> HasRepeatingPattern(int value)
+static Dictionary<string, long> HasRepeatingPattern(long value)
 {
-    Dictionary<string, int> repeatingDigits = new Dictionary<string, int>();
-    
-    for (int x = 0; x < value.ToString().Length - 1; x++)
+    Dictionary<string, long> repeatingDigits = new Dictionary<string, long>();
+
+    List<int> divideables = new List<int>
     {
-        for (int i = value.ToString().Substring(x).Length; i > 0; i--)
+        2, 3, 5, 7 // For part one, only add 2 to the list
+    };
+
+    foreach (var divideable in divideables)
+    {
+        int lenght = value.ToString().Length / divideable;
+
+        if (value.ToString().Length % divideable != 0)
         {
-            string substring = value.ToString().Substring(0, i);
-            if (repeatingDigits.ContainsKey(substring))
+            continue;
+        }
+        
+        List<string> values = new List<string>();
+        for (int i = 0; i < divideable; i++)
+        {
+            values.Add(value.ToString().Substring((lenght * i), lenght));
+        }
+
+        if (values.All(x => x == values[0]))
+        {
+            Console.WriteLine($"Repeating {value}");
+            if (repeatingDigits.ContainsKey(value.ToString()))
             {
-                repeatingDigits[substring]++;
+                repeatingDigits[value.ToString()]++;
+                break;
             }
             else
             {
-                repeatingDigits.Add(substring, 1);
+                repeatingDigits.Add(value.ToString(), 1);
+                break;
             }
         }
-    }
-
-    foreach (var digit in repeatingDigits.Values)
-    {
-        bool FoundDigit = false;
-        int startIndex =
-        while (!FoundDigit)
-        {
-            
-        }
-        value.ToString().Substring(0, digit);
     }
     
     return repeatingDigits;
@@ -58,6 +67,6 @@ static Dictionary<string, int> HasRepeatingPattern(int value)
 
 class ProcessIds
 {
-    public int StartingId = 0;
-    public int EndId = 0;
+    public long StartingId = 0;
+    public long EndId = 0;
 }
